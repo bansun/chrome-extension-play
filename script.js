98bc3581a6539401cff9f99c6baa000dd04488c7
Tinnei Pang <patanypang@berkeley.edu>
2015-10-10 14:05:40 -0700

    initial commit: base off @curtis inject-sidebar

---
+++ script.js
@@ -0,0 +1,45 @@
+/*Handle requests from background.html*/
+function handleRequest(
+	//The object data with the request params
+	request, 
+	//These last two ones isn't important for this example, if you want know more about it visit: http://code.google.com/chrome/extensions/messaging.html
+	sender, sendResponse
+	) {
+	if (request.callFunction == "toggleSidebar")
+		toggleSidebar();
+}
+chrome.extension.onRequest.addListener(handleRequest);
+
+/*Small function wich create a sidebar(just to illustrate my point)*/
+var sidebarOpen = false;
+function toggleSidebar() {
+	if(sidebarOpen) {
+		// hide it
+		var el = document.getElementById('mySidebar');
+		el.parentNode.removeChild(el);
+		sidebarOpen = false;
+	}
+	else {
+		var sidebar = document.createElement('div');
+		sidebar.id = "mySidebar";
+		sidebar.innerHTML = "\
+			<h1>Hello</h1>\
+			World!\
+		";
+		sidebar.style.cssText = "\
+			position:fixed;\
+			top:0px;\
+			left:50%;\
+			width:50%;\
+			height:100%;\
+			color: white;\
+			opacity: 0.5;\
+			background:black;\
+			border-radius:50px;\
+			box-shadow:inset 0 0 1em black;\
+			z-index:999999;\
+		";
+		document.body.appendChild(sidebar);
+		sidebarOpen = true;
+	}
+}
